4bea000a83e85f281622818a123104cf85a87fb2
mirzakhzm <150061311+mirzakhzm@users.noreply.github.com>
2023-12-10 18:14:05 +0700

    Update food.js

--- Documents/Vs Code/producteditan/food.js
+++ Documents/Vs Code/producteditan/food.js
@@ -31,29 +31,4 @@ router.get('/foods/:time_category', (req, res) => {
     });
   });
 
-//Endpoint Nyoba
-// const now = new Date();
-
-// Tentukan kategori makanan berdasarkan waktu saat ini
-// let time_category;
-// if (now.getHours() < 12) {
-//   time_category = 'Sarapan';
-// } else if (now.getHours() < 15) {
-//   time_category = 'Makan Siang';
-// } else {
-//   time_category = 'Makan Malam  ';
-// }
-
-// router.get('/foods/recommended', (req, res) => {
-//   const selectFoodsSql = 'SELECT * FROM food WHERE time_category = ? AND recommended = 1';
-
-//   pool.query(selectFoodsSql, [time_category], (error, results) => {
-//     if (error) {
-//       console.error('Error querying foods:', error);
-//       return res.status(500).json({ error: 'Error querying foods' });
-//     }
-//     res.json({ status: 'Success', food: results });
-//   });
-// });
-
 module.exports = router;
